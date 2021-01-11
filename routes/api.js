f15cf4fe63a8ab6b7d899a06feeef510a73f5c41
puakehaulani <puakehaulani@gmail.com>
2021-01-10 20:56:47 -0800

    created exporting structure of api routes

--- routes/api.js
+++ routes/api.js
@@ -7,3 +7,39 @@
 // exercise.html create workout POST /api/workouts
 
 // index.html add exercise(data) PUT /api/workouts/:id
+
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+// router.post("/api/workout", ({ body }, res) => {
+//   Workout.create(body)
+//     .then(dbWorkout => {
+//       res.json(dbWorkout);
+//     })
+//     .catch(err => {
+//       res.status(400).json(err);
+//     });
+// });
+
+// router.post("/api/workout/bulk", ({ body }, res) => {
+//   Workout.insertMany(body)
+//     .then(dbWorkout => {
+//       res.json(dbWorkout);
+//     })
+//     .catch(err => {
+//       res.status(400).json(err);
+//     });
+// });
+
+// router.get("/api/workout", (req, res) => {
+//   Workout.find({})
+//     .sort({ date: -1 })
+//     .then(dbWorkout => {
+//       res.json(dbWorkout);
+//     })
+//     .catch(err => {
+//       res.status(400).json(err);
+//     });
+// });
+
+module.exports = router;
