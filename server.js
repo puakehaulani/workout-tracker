1ac01adc9591899e6794f3e766ae26c22886f1ac
puakehaulani <puakehaulani@gmail.com>
2021-01-11 23:05:30 -0800

    server update, added routes

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
-const User = require("./models");
 const app = express();
 
 app.use(logger("dev"));
@@ -13,14 +12,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+app.use(require("./routes/apiroutes.js"));
+require("./routes/htmlroutes");
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
 });
-
-app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
