b45499a94cf134bd758a95201847705a898bff97
puakehaulani <puakehaulani@gmail.com>
2021-01-13 15:25:17 -0800

    tweaked model keys to match seed. completed put and post routes, funcitoning properly!!!!!!!!!!!!

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,38 +1,41 @@
-// ROUTES to mirror API.js
-
-// index.html get last workout GET /api/workouts
-
-// stats.html get workouts in range GET /api/workouts/range
-
-// exercise.html create workout POST /api/workouts
-
-// index.html add exercise(data) PUT /api/workouts/:id
 
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
-// router.post("/api/workouts", ({ body }, res) => {
-//   Workout.create(body)
-//     .then(dbWorkout => {
-//       res.json(dbWorkout);
-//     })
-//     .catch(err => {
-//       res.status(400).json(err);
-//     });
-// });
-
-// router.post("/api/workouts/bulk", ({ body }, res) => {
-//   Workout.insertMany(body)
-//     .then(dbWorkout => {
-//       res.json(dbWorkout);
-//     })
-//     .catch(err => {
-//       res.status(400).json(err);
-//     });
-// });
-
 router.get("/api/workouts", (req, res) => {
     Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+    const workoutId = req.params.id;
+    Workout.findOneAndUpdate({ _id: workoutId }, { $push: { exercises: req.body } }, { new: true })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+        // NEED TO: add in parameters for the range //
         .then(dbWorkout => {
             res.json(dbWorkout);
         })
